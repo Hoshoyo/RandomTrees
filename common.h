4e6e78bc0b9460927173a7b7f2f864c73b5baa58
Hoshoyo <pveiga1991@hotmail.com>
2017-12-10 02:40:19 -0200

    Initial commit. Ganho de informação, algorithm implemented with an example.

---
+++ common.h
@@ -0,0 +1,15 @@
+#pragma once
+typedef unsigned long long u64;
+typedef unsigned int u32;
+typedef unsigned short u16;
+typedef unsigned char u8;
+typedef long long s64;
+typedef int s32;
+typedef short s16;
+typedef char s8;
+
+typedef float r32;
+typedef double r64;
+
+#include <stdio.h>
+#include <math.h>
